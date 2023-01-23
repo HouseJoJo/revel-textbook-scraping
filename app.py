9e19f38d97248c8ce7f72ab772a0a3191ba5928d
Josue S <68451593+HouseJoJo@users.noreply.github.com>
2023-01-23 14:48:54 -0500

    Add nativation to Chapter 1.1 of revel & comments

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ xpaths = { 'openRevel' : "/html/body/div[1]/main/div[2]/div/div[2]/div/div[2]/di
     'appPopUpClose' : "/html/body/appcues/div[2]/a",
     'osPopupClose': "/html/body/div[5]/div[2]/div/div[1]/button",
     'courseContentInput': "/html/body/div[2]/div/div[2]/div[4]/div[3]/div[1]/div[3]/div[2]/div/div",
-    'ccInputChapters': "/html/body/div[5]/div[3]/ul/li[2]/div"}
+    'ccInputChapters': "/html/body/div[5]/div[3]/ul/li[2]/div",
+    'chapter1Selection': '//*[@id="urn:pearson:entity:45f74f4e-81e4-43cf-8e32-0e21f775a565"]',
+    'chapter1-1Selection': "/html/body/div[2]/div/div[2]/div[4]/div[3]/div[2]/div/ul/li[4]/div/div/div/ul/li[1]/button" }
     #Pairs of xpaths for future use.
 
 driver = webdriver.Firefox()
@@ -44,23 +46,31 @@ WebDriverWait(driver, 30).until(
     EC.title_is("History 1111 - Spring 2023 - Dashboard") #Waits for page to load
 )
 print("Revel Content loaded")
+
 try:
-    driver.find_element(By.XPATH,xpaths['appPopUpClose']).click()
+    driver.find_element(By.XPATH,xpaths['appPopUpClose']).click() #Block closes a potential app pop-up
     print("App popup closed")
 except NoSuchElementException:
     print("App popup not found")
 
 try:
-    driver.find_element(By.XPATH,xpaths["osPopupClose"]).click()
+    driver.find_element(By.XPATH,xpaths["osPopupClose"]).click() #Code block closes any "incompatible OS" pop-ups
     print("OS popup closed")
 except NoSuchElementException:
     print("OS popup not found")
 
 print("Final")
 WebDriverWait(driver, 30).until(
-    EC.presence_of_element_located((By.XPATH,xpaths["courseContentInput"]))
+    EC.presence_of_element_located((By.XPATH,xpaths["courseContentInput"])) #Waits for new page to load.
 )
 driver.find_element(By.XPATH,xpaths["courseContentInput"]).click()
+driver.find_element(By.XPATH,xpaths["ccInputChapters"]).click() #Selects Chapter view on webpage
 
-driver.find_element(By.XPATH,xpaths["ccInputChapters"]).click()
 print("All chapters selected")
+
+driver.find_element(By.XPATH,xpaths['chapter1Selection']).click()
+driver.find_element(By.XPATH,xpaths["chapter1-1Selection"]).click() #Locates and enters Chapter 1.1
+print("Entered chapter 1.1")
+
+#From this point forward, we should selects all titles, headers, and paragraph contents from page and
+#compile them into a document. Either create an array for each chapter, and an array to compile all chapters.
