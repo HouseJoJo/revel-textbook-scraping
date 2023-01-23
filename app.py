d76ff8c4fd91f91e4febacee4f812e89bcc981da
Josue S <68451593+HouseJoJo@users.noreply.github.com>
2023-01-23 13:00:16 -0500

    Add try blocks to close popups to content

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 xpaths = { 'openRevel' : "/html/body/div[1]/main/div[2]/div/div[2]/div/div[2]/div/div[4]/a[2]",
     'openRevelAlt' : "/html/body/div[1]/main/div[2]/div/div[2]/div/div[2]/div/div[4]/a[2]/span",
     'closeAlert1Bttn' : "/html/body/div[5]/div[2]/div/div[1]/button",
-    'continueReadingBttn' : "/html/body/div[2]/div/div[2]/div[4]/div[1]/div/div[1]/div/div[2]/div/div/div/button" }
+    'continueReadingBttn' : "/html/body/div[2]/div/div[2]/div[4]/div[1]/div/div[1]/div/div[2]/div/div/div/button",
+    'appPopUp' : "/html/body/appcues",
+    'appPopUpClose' : "/html/body/appcues/div[2]/a",
+    'OSPopup': "/html/body/div[5]/div[2]",
+    'OSPopupClose': "/html/body/div[5]/div[2]/div/div[1]/button"}
     #Pairs of xpaths for future use.
 
 driver = webdriver.Firefox()
@@ -30,10 +35,23 @@ loginbttn.click()
 WebDriverWait(driver, 30).until(
     EC.presence_of_element_located((By.XPATH,xpaths["openRevel"])) #wait for browser to load next page
 )
+print("Login success")
 
 driver.find_element(By.XPATH,xpaths["openRevel"]).click() #Clicks to open assignment
 
 WebDriverWait(driver, 30).until(
     EC.title_is("History 1111 - Spring 2023 - Dashboard") #Waits for page to load
 )
-print("Out")
+print("Revel Content loaded")
+try:
+    driver.find_element(By.XPATH,xpaths['appPopUpClose']).click()
+except NoSuchElementException:
+    print("App popup not found")
+
+try:
+    driver.find_element(By.XPATH,xpaths["OSPopupClose"]).click()
+    print("OS popup closed")
+except NoSuchElementException:
+    print("OS popup not found")
+
+print("Final")
